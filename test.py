45184bcbbd5cb610394331edbda7e267f9b952aa
vaibhavk1992 <kumarvaibhav1992@ymail.com>
2017-05-07 02:01:39 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 23:47:22 2017
+
+@author: vaibhav
+"""
+
+class createnode:
+    def __init__(self,data):
+        self.root=data
+        self.left=None
+        self.right=None
+    
+class createbinarytree:
+
+    def array(self):
+        self.q=[]
+        self.num=int(raw_input("enter the number of element you want in the list"))
+        for i in range(0,self.num):   
+          self.num1=int(raw_input("enter the " + `i+1` +"element"))
+          self.q.append(self.num1)
+    
+    def createtree(self):
+        self.l=[1,2,3,4,5]
+        arr_len=len(self.l)
+        k=0
+ 
+        while(k<arr_len):
+           root_data=self.l[k]
+           self.top=createnode(root_data)
+           self.x=int(2) * int(k)+int(1)
+           if(self.x>=arr_len):
+             break;  
+           left_data=self.l[self.x]
+           self.left_child=createnode(left_data)
+           
+           self.y=int(2) * int(k)+int(2)
+           if(self.y>=arr_len):
+             break;  
+           right_data=self.l[self.y]
+           self.right_child=createnode(right_data)
+           self.top.left=self.left_child
+           self.top.right=self.right_child
+           print(self.top.left.root)
+           print(self.top.right.root)
+           k=k+1
+    def inordertraversse(self,top):
+           if(top):
+            self.inordertraversse(top.left)
+            print(top.root)
+            self.inordertraversse(top.right)
+            
+            
+
+        
+
+conv=createbinarytree();
+#conv.array()
+conv.createtree()
+conv.inordertraversse(conv.top)
+
+
